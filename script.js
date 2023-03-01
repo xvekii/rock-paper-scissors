86a3a4f2115354f7422dcb88a880f8c13d72a047
Vedran D <vedran.dautanec@gmail.com>
2023-03-01 20:17:58 +0100

    Add toLowerCase to playRound function

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ function getComputerChoice(max) {
 
 console.log(getComputerChoice(3));
 
+// Plays a round and announces the winner
 function playRound(playerSelection, computerSelection) {
   
+  // arrange the case insensitivenesss
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  
+  
+  // 1st condition if pS == R && cS == P
+  
+  // return "You Lose! Paper beats Rock"
 }

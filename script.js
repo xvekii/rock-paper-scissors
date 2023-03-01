d7b6de51749967ce562133f19c4c9b12736c0fbe
Vedran D <vedran.dautanec@gmail.com>
2023-03-01 19:01:42 +0100

    Add script.js, add function getComputerChoice to emulate the computer

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello, vekii!");
+
+// "Computer": randomly returns Rock Paper Scissors
+let arr = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(max) {
+  let temp = Math.floor(Math.random() * max);
+  return arr[temp];
+}
+
+console.log(getComputerChoice(3));
